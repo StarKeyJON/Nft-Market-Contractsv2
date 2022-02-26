4650424464089d8c762e1d271109213fb22d28aa
Ken Preiser <kenpreiser@gmail.com>
2022-02-25 22:21:57 -0800

    Create ERC721AFactory.sol

---
+++ ERC721AFactory.sol
@@ -0,0 +1,100 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./Pham721A.sol";
+
+interface RoleProvider {
+  function hasTheRole(bytes32 role, address _address) external returns(bool);
+}
+
+contract ERC721AFactory {
+  using Counters for Counters.Counter;
+
+  //*~~~> counter increments 
+  Counters.Counter private _nftContractsCreated;
+
+  //*~~~> Declaring object structures for NFT contracts
+  struct NFTContract {
+    bool is1155;
+    uint itemId;
+    address creator;
+  }
+
+  constructor(address _role){
+    roleAdd = _role;
+  }
+
+  address public roleAdd;
+
+  //*~~~> Memory array of all NFT contracts created
+  mapping (uint256 => NFTContract) private _idToNftContract;
+
+  /*~~~>
+    Roles for designated accessibility
+  <~~~*/
+  bytes32 public constant PROXY_ROLE = keccak256("PROXY_ROLE"); 
+  modifier hasAdmin(){
+    require(RoleProvider(roleAdd).hasTheRole(PROXY_ROLE, msg.sender), "DOES NOT HAVE ADMIN ROLE");
+    _;
+  }
+  bytes32 public constant CONTRACT_ROLE = keccak256("CONTRACT_ROLE");
+  modifier hasContractAdmin(){
+    require(RoleProvider(roleAdd).hasTheRole(CONTRACT_ROLE, msg.sender), "DOES NOT HAVE CONTRACT ROLE");
+    _;
+  }
+  // *~~~> Events declared for listeners
+  //constructor(address userOwnerAddress, address controllerAddress, address minterAddress, uint256 _price, uint256 _supply, string memory tokenName, string memory _tokenSymbol) 
+  event nftContractCreated(address newAddress, uint nftId, address creator);
+
+  function setRoleAdd(address _role) public hasAdmin returns(bool){
+    roleAdd = _role;
+    return true;
+  }
+
+  ///@notice
+  //*~~~> Function for deploying new ERC-721 contracts
+  ///@dev
+  /*~~~> 
+    address controller: controller operator chosen by the user;
+    address minter: minter role chosen byu the user;
+    string calldata name: name of the contract;
+    string calldata symbol: symbol of the token;
+  <~~~*/ 
+  function newNftContract(address userOwnerAddress, address controllerAddress, address minterAddress, uint256 _price, uint256 _supply, string memory tokenName, string memory _tokenSymbol) hasContractAdmin public payable returns(bool) {
+    PhamNFTs c = new Pham721A(userOwnerAddress, controllerAddress, minterAddress, _price, _supply, tokenName, _tokenSymbol);
+    _nftContractsCreated.increment();
+    uint256 nftId = _nftContractsCreated.current();
+    _idToNftContract[nftId] = NFTContract(false, nftId, msg.sender);
+    emit nftContractCreated(address(c), nftId, msg.sender);
+    return true;
+  }
+
+  //*~~~> Read functions for internal state
+  function getContractCount() public view returns(uint) {
+    return _nftContractsCreated.current();
+  }
+
+  function fetchNFTContractsCreated() public view returns (NFTContract[] memory) {
+    uint itemCount = _nftContractsCreated.current();
+    NFTContract[] memory contracts = new NFTContract[](itemCount);
+    uint currentIndex;
+    for (uint i; i < itemCount; i++) {
+        NFTContract storage currentItem = _idToNftContract[i + 1];
+         contracts[currentIndex] = currentItem;
+         currentIndex++;
+    }
+    return contracts;
+  }
+
+  /*~~~>
+  Fallback functions
+  <~~~*/
+  ///@notice
+  /*~~~> External ETH transfer forwarded to role provider contract <~~~*/
+  event FundsForwarded(uint value, address _from, address _to);
+  receive() external override payable {
+    payable(roleAdd).transfer(msg.value);
+      emit FundsForwarded(msg.value, msg.sender, roleAdd);
+  }
+}
